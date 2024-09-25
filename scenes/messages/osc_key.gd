cd286f1b449d044fc83da00516ee0bd274c29287
Apollo Nargang <CoffeeCoder1@outlook.com>
2024-09-24 21:12:39 -0400

    Reduce the threshold for font resizing 5-character strings were extending past the button border.

--- scenes/messages/osc_key.gd
+++ scenes/messages/osc_key.gd
@@ -7,7 +7,7 @@ class_name OSCKey extends Button
 		# Iterate through label lines
 		for line in new_label.split("\n"):
 			# Find a good font size, and, if it's smaller than the current one, set it as the font size override
-			var font_size: int = 12 - ((line.length() - 6) * 1.1)
+			var font_size: int = 12 - ((line.length() - 5) * 1.05)
 			if font_size <= get_theme_font_size("font_size"):
 				add_theme_font_size_override("font_size", font_size)
 			else:
