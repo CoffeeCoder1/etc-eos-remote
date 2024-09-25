9e615f43053832758d043e1f62ab643b5fa2c8be
Apollo Nargang <CoffeeCoder1@outlook.com>
2024-09-24 20:53:15 -0400

    Resize fonts to fit buttons dynamically

--- scenes/messages/osc_key.gd
+++ scenes/messages/osc_key.gd
@@ -11,6 +11,17 @@ func _ready() -> void:
 	button_up.connect(_on_input.bind(false))
 
 
+func _process(delta: float) -> void:
+	# Iterate through label lines
+	for line in text.split("\n"):
+		# Find a good font size, and, if it's smaller than the current one, set it as the font size override
+		var font_size: int = 12 - ((line.length() - 6) * 1.1)
+		if font_size <= get_theme_font_size("font_size"):
+			add_theme_font_size_override("font_size", font_size)
+		else:
+			remove_theme_font_size_override("font_size")
+
+
 func _on_input(is_pressed: bool) -> void:
 	target_user.send_message(command + "/" + key_string, [float(is_pressed)])
 	
