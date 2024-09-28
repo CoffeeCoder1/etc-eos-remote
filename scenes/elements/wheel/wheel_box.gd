bf1dcb16543ede26946339f8b1de85b8a79c39ce
Apollo Nargang <CoffeeCoder1@outlook.com>
2024-09-27 20:16:42 -0400

    Adjust wheel sensitivity Makes the wheel a bit slower.

--- scenes/elements/wheel/wheel_box.gd
+++ scenes/elements/wheel/wheel_box.gd
@@ -7,7 +7,7 @@ signal released
 func _gui_input(event: InputEvent):
 	if event is InputEventMouseMotion:
 		if event.button_mask != 0:
-			dragged.emit((get_global_rect().get_center().y - event.global_position.y) * 8 / size.y)
+			dragged.emit((get_global_rect().get_center().y - event.global_position.y) / size.y)
 	elif event is InputEventMouseButton:
 		if not event.is_pressed():
 			released.emit()
