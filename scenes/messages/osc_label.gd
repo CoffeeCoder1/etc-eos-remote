b7b12908971d42c2bc667c3479f5dfc027c3ccea
Apollo Nargang <CoffeeCoder1@outlook.com>
2024-09-29 18:55:33 -0400

    Add a label for the current show name

--- scenes/messages/osc_label.gd
+++ scenes/messages/osc_label.gd
@@ -1,5 +1,6 @@
-extends Label
+class_name OSCLabel extends Label
 
+@export var global_feedback: bool
 @export var feedback_address: String
 
 var osc_element: OSCElement
@@ -8,8 +9,10 @@ var osc_element: OSCElement
 func _ready() -> void:
 	osc_element = OSCElement.new()
 	add_child(osc_element)
+	
 	osc_element.recieve_address = feedback_address
 	osc_element.feedback_recieved.connect(_on_osc_feedback)
+	osc_element.global_feedback = global_feedback
 
 
 func _on_osc_feedback(value):
