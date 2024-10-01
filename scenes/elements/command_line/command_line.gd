cb31ed64bb2f1daf1e8e1cdd752acf337e9c8561
Apollo Nargang <CoffeeCoder1@outlook.com>
2024-09-30 20:16:13 -0400

    Add syntax highlighting to the command line The implementation is still not perfect, but everything needed to make it work is implemented so the only thing needed now is just to improve the regex a bit.

---
+++ scenes/elements/command_line/command_line.gd
@@ -0,0 +1,19 @@
+extends TextEdit
+
+@export var feedback_mode: OSCElement.FeedbackMode = OSCElement.FeedbackMode.USER
+@export var feedback_address: String
+
+var osc_element: OSCElement
+
+
+func _ready() -> void:
+	osc_element = OSCElement.new()
+	add_child(osc_element)
+	
+	osc_element.recieve_address = feedback_address
+	osc_element.feedback_recieved.connect(_on_osc_feedback)
+	osc_element.feedback_mode = feedback_mode
+
+
+func _on_osc_feedback(value: Array):
+	text = value[0]
