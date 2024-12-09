6492d15889116050cc32d918b83ce67a76883ae5
Apollo Nargang <CoffeeCoder1@outlook.com>
2024-12-09 15:41:49 -0500

    Use onready vars in main.gd

--- scenes/main/main.gd
+++ scenes/main/main.gd
@@ -1,19 +1,23 @@
 extends Control
 
+@onready var settings_container: PanelContainer = %SettingsContainer
+@onready var osc_user: OSCUser = $OSCUser
+@onready var osc_client_tcp: OSCClientTCP = $OSCClientTCP
+
 
 func _ready() -> void:
-	%SettingsContainer.set_ip_address($OSCClientTCP.ip_address)
-	%SettingsContainer.set_port($OSCClientTCP.port)
-	%SettingsContainer.set_user($OSCUser.user_number)
+	settings_container.set_ip_address(osc_client_tcp.ip_address)
+	settings_container.set_port(osc_client_tcp.port)
+	settings_container.set_user(osc_user.user_number)
 
 
 func _on_settings_user_selected(user_number: int) -> void:
-	$OSCUser.user_number = user_number
+	osc_user.user_number = user_number
 
 
 func _on_settings_ip_address_selected(ip_address: String) -> void:
-	$OSCClientTCP.ip_address = ip_address
+	osc_client_tcp.ip_address = ip_address
 
 
 func _on_settings_port_selected(port: int) -> void:
-	$OSCClientTCP.port = port
+	osc_client_tcp.port = port
