d9ab132ee0a65a02b173c015ddff3df8ccdef01f
Apollo Nargang <CoffeeCoder1@outlook.com>
2025-01-12 21:31:41 -0500

    Use _gui_input to handle color palette inputs This fixes some bugs relating to the color palette picking up inputs it should not have. Also contains some other miscellaneous small fixes.

--- scenes/elements/color_palette/color_palette.gd
+++ scenes/elements/color_palette/color_palette.gd
@@ -18,43 +18,22 @@ var last_color: Vector2
 var start_position: Vector2
 ## Used to time when a click and drag has started.
 var click_and_drag_timer: float
+## Is the palette actively being clicked?
+var clicked: bool
 ## Was the click and drag canceled (drag and snap)?
 var snap_dragging: bool
 var drag_started: bool
 
 
 func _process(delta: float) -> void:
-	if Input.is_action_pressed("palette_click"):
-		var mouse_position := get_local_mouse_position()
-		## Mouse position scaled to the size of the palette.
-		var color_position := get_local_mouse_position() / size
-		
-		# Check for the start of a click and drag
-		if Input.is_action_just_pressed("palette_click"):
-			start_position = color_position
-			relative_cursor.position = start_position * size
-		
+	var mouse_position := get_local_mouse_position()
+	## Mouse position scaled to the size of the palette.
+	var color_position := mouse_position / size
+	
+	if clicked:
 		# Find distance dragged
 		var position_difference := (color_position - start_position)
 		var drag_distance: float = abs(position_difference.x) + abs(position_difference.y)
-		
-		# Check if mouse click is inside the color palette
-		if mouse_position.x > 0 and mouse_position.y > 0 and mouse_position.x < size.x and mouse_position.y < size.y:
-			# Select drag mode
-			if !drag_started:
-				# If dragged far enough, use snap dragging
-				if drag_distance > snap_drag_threshold:
-					snap_dragging = true
-					drag_started = true
-					relative_cursor.hide()
-				
-				# If held long enough, use relative dragging
-				if click_and_drag_timer > snap_drag_window:
-					snap_dragging = false
-					drag_started = true
-					relative_cursor.show()
-				
-				click_and_drag_timer += delta
 		
 		if drag_started:
 			# Snap dragging
@@ -63,14 +42,50 @@ func _process(delta: float) -> void:
 			# Relative dragging
 			else:
 				color += position_difference * relative_movement_speed * delta
+		# Select drag mode
+		else:
+			# If dragged far enough, use snap dragging
+			if drag_distance > snap_drag_threshold:
+				snap_dragging = true
+				drag_started = true
+				relative_cursor.hide()
+			
+			# If held long enough, use relative dragging
+			if click_and_drag_timer > snap_drag_window:
+				snap_dragging = false
+				drag_started = true
+				relative_cursor.show()
+			
+			click_and_drag_timer += delta
+		
 		color = color.clamp(Vector2.ZERO, Vector2.ONE)
-	else:
-		# End drag and reset things
-		relative_cursor.hide()
-		click_and_drag_timer = 0
-		drag_started = false
+	
 	cursor.position = color * size
 	
+	# If the color changed, emit a signal saying so
 	if color != last_color:
 		color_changed.emit(color)
 	last_color = color
+
+
+func _gui_input(event: InputEvent) -> void:
+	var mouse_position: Vector2 = get_local_mouse_position()
+	## Mouse position scaled to the size of the palette.
+	var color_position := mouse_position / size
+	
+	if event is InputEventMouseButton:
+		if event.is_pressed():
+			# Start drag
+			clicked = true
+			start_position = color_position
+			relative_cursor.position = start_position * size
+		else:
+			# End drag and reset things
+			clicked = false
+			relative_cursor.hide()
+			click_and_drag_timer = 0
+			drag_started = false
+
+## Sets the color.
+func set_color(new_color: Vector2) -> void:
+	color = new_color
