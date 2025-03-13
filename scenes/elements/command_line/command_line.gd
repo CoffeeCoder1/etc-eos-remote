4e107756cafffe74a3f55271284cf2b84f9f9f22
Apollo Nargang <CoffeeCoder1@outlook.com>
2025-03-13 19:59:22 -0400

    Set up the CommandLine in its class and delete its scene

--- scenes/elements/command_line/command_line.gd
+++ scenes/elements/command_line/command_line.gd
@@ -1,18 +1,23 @@
-extends TextEdit
+class_name CommandLine extends TextEdit
 
-@export var feedback_mode: OSCElement.FeedbackMode = OSCElement.FeedbackMode.USER
-@export var feedback_address: String
+@export var feedback_mode: OSCElement.FeedbackMode = OSCElement.FeedbackMode.FEEDBACK_USER
+@export var feedback_address: String = "/cmd"
+
+const COMMAND_LINE_SYNTAX_HIGHLIGHTER = preload("res://scenes/elements/command_line/command_line_syntax_highlighter.tres")
 
 var osc_element: OSCElement
 
 
-func _ready() -> void:
+func _init() -> void:
 	osc_element = OSCElement.new()
 	add_child(osc_element, false, Node.INTERNAL_MODE_FRONT)
-	
 	osc_element.recieve_address = feedback_address
 	osc_element.feedback_recieved.connect(_on_osc_feedback)
 	osc_element.feedback_mode = feedback_mode
+
+
+func _ready() -> void:
+	syntax_highlighter = COMMAND_LINE_SYNTAX_HIGHLIGHTER
 
 
 func _on_osc_feedback(value: Array):
