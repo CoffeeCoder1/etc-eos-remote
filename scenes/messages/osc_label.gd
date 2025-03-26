1377423d41919bba305986857a4df83ecd38371c
Apollo Nargang <CoffeeCoder1@outlook.com>
2025-03-26 18:17:42 -0400

    Set the OSCLabel's feedback address in a setter
    Allows for changing the feedback address after creating the label.

--- scenes/messages/osc_label.gd
+++ scenes/messages/osc_label.gd
@@ -1,15 +1,21 @@
 class_name OSCLabel extends Label
 
 @export var feedback_mode: OSCElement.FeedbackMode = OSCElement.FeedbackMode.USER
-@export var feedback_address: String
+@export var feedback_address: String:
+	set(new_feedback_address):
+		if is_instance_valid(osc_element):
+			osc_element.recieve_address = new_feedback_address
+			feedback_address = new_feedback_address
 
 var osc_element: OSCElement
 
 
-func _ready() -> void:
+func _init() -> void:
 	osc_element = OSCElement.new()
 	add_child(osc_element, false, Node.INTERNAL_MODE_FRONT)
-	
+
+
+func _ready() -> void:
 	osc_element.recieve_address = feedback_address
 	osc_element.feedback_recieved.connect(_on_osc_feedback)
 	osc_element.feedback_mode = feedback_mode
