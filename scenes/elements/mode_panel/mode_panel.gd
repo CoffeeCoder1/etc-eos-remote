1c4fa9bf847e91478c55c91f8a99b06f4ae84146
Apollo Nargang <CoffeeCoder1@outlook.com>
2025-03-26 22:58:19 -0400

    Display the live color when disconnected
    Pretty jank but it works fine. Might improve this a bit later, but the
    way it is now should be pretty easy to un-jank once I get the OSC
    refactoring that I've been meaning to do done.

--- scenes/elements/mode_panel/mode_panel.gd
+++ scenes/elements/mode_panel/mode_panel.gd
@@ -7,30 +7,53 @@ class_name ModePanel extends PanelContainer
 
 var color_tween: Tween
 var last_state: Array
+var disconnected: bool = true
+
+
+func _ready() -> void:
+	OSCGlobals.disconnected.connect(_on_osc_disconnected)
+	OSCGlobals.connected.connect(_on_osc_connected)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	var state: Array = OSCGlobals.get_client().get_incoming_messages().get("/eos/out/event/state", [])
+	var state: Array = OSCGlobals.get_client().get_incoming_messages().get("/eos/out/event/state", [1.0])
+	
+	if disconnected:
+		state[0] = 1.0
+	
 	# Has the state changed?
 	if state != last_state:
-		# Remove old Tweens
-		if color_tween:
-			color_tween.kill()
-		
-		# Create a Tween for the color
-		color_tween = get_tree().create_tween()
-		
-		# Check what state the board is in
 		if state[0] == 1.0:
-			color_tween.tween_method(_set_bg_color, get_theme_stylebox("panel").get("bg_color"), live_color, 0.5)
+			fade_bg_color(live_color)
 		else:
-			color_tween.tween_method(_set_bg_color, get_theme_stylebox("panel").get("bg_color"), blind_color, 0.5)
-		
+			fade_bg_color(blind_color)
 		last_state = state
 
 
+## Sets the background color immediately
 func _set_bg_color(bg_color: Color) -> void:
 	var style_box: StyleBoxFlat = get_theme_stylebox("panel")
 	style_box.set("bg_color", bg_color)
 	add_theme_stylebox_override("panel", style_box)
+
+
+## Fades the background color to the given color.
+func fade_bg_color(bg_color: Color) -> void:
+	# Remove old Tweens
+	if color_tween:
+		color_tween.kill()
+	
+	# Create a Tween for the color
+	color_tween = get_tree().create_tween()
+	
+	# Check what state the board is in
+	color_tween.tween_method(_set_bg_color, get_theme_stylebox("panel").get("bg_color"), bg_color, 0.5)
+
+
+func _on_osc_disconnected() -> void:
+	disconnected = true
+
+
+func _on_osc_connected() -> void:
+	disconnected = false
