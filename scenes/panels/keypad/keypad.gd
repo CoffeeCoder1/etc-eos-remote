f6dbd5b799fe38828f0e7eb7a7f9b3495f6139d4
Apollo Nargang <CoffeeCoder1@outlook.com>
2025-04-14 19:28:29 -0400

    Keep keypad key aspect ratios when scaling
    Kinda jank but it works well enough for now.

--- scenes/panels/keypad/keypad.gd
+++ scenes/panels/keypad/keypad.gd
@@ -2,11 +2,11 @@ class_name Keypad extends PanelContainer
 
 @onready var tab_bar: TabBar = %TabBar
 @onready var key_panels: Control = %KeyPanels
-@onready var v_box_container: VBoxContainer = %VBoxContainer
-@onready var v_box_container_2: VBoxContainer = %VBoxContainer2
-@onready var v_box_container_3: VBoxContainer = %VBoxContainer3
-@onready var v_box_container_4: VBoxContainer = %VBoxContainer4
-@onready var v_box_container_5: VBoxContainer = %VBoxContainer5
+@onready var panel_1: AspectRatioContainer = %Panel1
+@onready var panel_2: AspectRatioContainer = %Panel2
+@onready var panel_3: AspectRatioContainer = %Panel3
+@onready var panel_4: AspectRatioContainer = %Panel4
+@onready var panel_5: AspectRatioContainer = %Panel5
 @onready var wheel: Wheel = %Wheel
 
 enum Tabs {
@@ -25,11 +25,11 @@ func _on_rect_changed() -> void:
 	tab_bar.visible = size.aspect() < 1.313
 	
 	if size.aspect() < 1.313:
-		v_box_container.set_meta("tab", Tabs.TARGET)
-		v_box_container_5.set_meta("tab", Tabs.TARGET)
+		panel_1.set_meta("tab", Tabs.TARGET)
+		panel_5.set_meta("tab", Tabs.TARGET)
 	else:
-		v_box_container.set_meta("tab", Tabs.MAIN)
-		v_box_container_5.set_meta("tab", Tabs.MAIN)
+		panel_1.set_meta("tab", Tabs.MAIN)
+		panel_5.set_meta("tab", Tabs.MAIN)
 	
 	queue_redraw()
 
